8f1c6107a811127b32fb3d615b4070f67ddeced0
EnriqueTJ-11 <enriquetst2004@gmail.com>
2025-11-16 14:44:27 -0500

    prueba3

--- src/pages/LaboratoriesManagement.tsx
+++ src/pages/LaboratoriesManagement.tsx
@@ -193,14 +193,14 @@ export default function LaboratoriesManagement() {
         
         <div className="min-h-screen bg-gray-50">
             <LabModal
-                            isOpen={isModalOpen}
-                            onClose={handleCloseModal}
-                            onSave={handleSaveLab}
-                            editingLab={editingLab}
-                        />
-                        
+                isOpen={isModalOpen}
+                onClose={handleCloseModal}
+                onSave={handleSaveLab}
+                editingLab={editingLab}
+            />
+
             <Navbar />
-            
+
             <div>
                 <div className="max-w-7xl mx-auto p-6">
                     {/* Header */}
