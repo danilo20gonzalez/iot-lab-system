783c3d4006d503bf9f4309077c0b6b41d7bccce7
EnriqueTJ-11 <enriquetst2004@gmail.com>
2026-03-19 21:46:40 -0500

    error solucionado del app por un commit viejo en la rama frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,37 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import UsersManagement from "./pages/UsersManagement";
-import Laboratory from "./pages/Laboratory"
-import Project from "./pages/Projects"
-import Stand from "./pages/Stands"
-import Row from "./pages/Rows"
-
+import Laboratory from "./pages/Laboratory";
+import LaboratoriesManagement from "./pages/LaboratoriesManagement";
+import Project from "./pages/Projects";
+import Stand from "./pages/Stands";
+import Row from "./pages/Rows";
+import Estantes3D from "./pages/Estantes3D";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/users" element={<UsersManagement />} />
-        <Route path="/laboratory" element={<Laboratory />} />
-        <Route path="/project" element={<Project />} />
-        <Route path="/stand" element={<Stand />} />
-        <Route path="/row" element={<Row />} />
-
-        {/* Ruta detalle de laboratorio */}
-        <Route path="/laboratorios/:id" element={<h1>Detalle de laboratorio</h1>} />
-      </Routes>
-    </Router>
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route element={<ProtectedRoute />}>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/users" element={<UsersManagement />} />
+            <Route path="/laboratory" element={<Laboratory />} />
+            <Route path="/laboratories-management" element={<LaboratoriesManagement />} />
+            <Route path="/project" element={<Project />} />
+            <Route path="/stand" element={<Stand />} />
+            <Route path="/row" element={<Row />} />
+            <Route path="/estante-3d" element={<Estantes3D />} />
+            {/* Ruta detalle de laboratorio */}
+            <Route path="/laboratorios/:id" element={<h1>Detalle de laboratorio</h1>} />
+          </Route>
+        </Routes>
+      </Router>
+    </AppProvider>
   );
 }
 
