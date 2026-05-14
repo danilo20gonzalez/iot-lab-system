22ca6360f954cca9ead0f884442942cc71a71819
DanielSuar3z <danielsuarez2407@gmail.com>
2026-05-14 12:01:54 -0500

    Corrección en el tipado de  RealTimeCamera y eliminación de importaciones innecesarias en Laboratory.tsx

--- frontend/src/pages/Laboratory.tsx
+++ frontend/src/pages/Laboratory.tsx
@@ -12,15 +12,9 @@ import type { ComponentData } from "../context/AppContext";
 import { ReactSortable } from 'react-sortablejs';
 import LabRoomCard from '../components/LabRoomCard';
 import CreateSalaModal from '../modals/CreateModuloModal';
-import UserModal from '../modals/UserModal';
 import api from '../api/api';
 import { wsUrl } from '../../config';
 
-interface LabDevice {
-  id: string;
-  type: string;
-  name: string;
-}
 
 const Laboratory = () => {
   const { id } = useParams<{ id: string }>();
